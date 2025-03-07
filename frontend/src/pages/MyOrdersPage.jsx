4b701841665d6b0c607837c90862efa81c79c1c4
saketkakkar-101 <saketkakkar55555@gmail.com>
2025-03-07 18:47:29 +0530

    commit

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -47,7 +47,41 @@ const MyOrdersPage = () => {
       <h2 className='text-xl sm:text-2xl font-bold mb-6'>
         My Orders
       </h2>
-      <div className='relative shadow-md sm:rounded-lg overflow-hidden'></div>
+      <div className='relative shadow-md sm:rounded-lg overflow-hidden'>
+        <table className='min-w-full text-left text-gray-500'>
+         <thead className='bg-gray-100 text-xs uppercase text-gray-700'> 
+           <tr>
+            <th className='py-2 px-4 sm:py-3'>Image</th>
+            <th className='py-2 px-4 sm:py-3'>Order ID</th>
+            <th className='py-2 px-4 sm:py-3'>Created</th>
+            <th className='py-2 px-4 sm:py-3'>shiiping Address</th>
+            <th className='py-2 px-4 sm:py-3'>Items</th>
+            <th className='py-2 px-4 sm:py-3'>Price</th>
+            <th className='py-2 px-4 sm:py-3'>Status</th>
+           </tr>
+         </thead>
+         <tbody>
+          {orders.length > 0 ? (
+            orders.map((order) => (
+            <tr key={order._id} className='border-b hover:border-gray-50 cursor-pointer'>
+              <td className='py-2 px-2 sm:py-4 sm:px-4'>
+                <img src={order.orderItems[0].image} alt={order.orderItems[0].name}
+                className='w-10 h-10 sm:w-12 sm:h-12 object-cover rounded-lg' />
+              </td>
+            </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={7}
+              className='py-4 px-4 text-center text-gray-500'
+              >
+                You have no orders
+              </td>
+            </tr>
+          )}
+         </tbody>
+        </table>
+      </div>
     </div>
   )
 }
