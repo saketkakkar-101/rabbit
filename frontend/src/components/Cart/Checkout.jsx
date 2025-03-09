729fe74f861c135d4564abfd8e096284d256eda5
saketkakkar-101 <saketkakkar55555@gmail.com>
2025-03-09 19:00:34 +0530

    commit

--- frontend/src/components/Cart/Checkout.jsx
+++ frontend/src/components/Cart/Checkout.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import PayPalButton from './PayPalButton';
 
 const cart ={ 
 
@@ -23,6 +24,7 @@ totalPrice: 195,
 }
 const Checkout = () => {
     const navigate = useNavigate();
+    const [checkoutId , setCheckoutId] = useState(null) 
     const [shippingAddress, setShippingAddress] = useState({
         firstName : "",
         lastName : "",
@@ -32,12 +34,24 @@ const Checkout = () => {
         country: "",
         phone: "",
     })
+
+    const handleCreateCheckout = (e) => {
+        e.preventDefault();
+        // setCheckoutId(1234)
+    }
+
+const handlePaymentSuccess = (details) => {
+    console.log("Payment Successful" , details);
+    navigate("/order-confirmation")
+    
+}
+
   return (
     <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto py-10 px-6 tracking-tighter'>
             {/* Left Section */}
        <div className='bg-white rounded-lg p-6'>
        <h2 className='text-2xl uppercase mb-6'>Checkout</h2>
-        <form>
+        <form onSubmit={handleCreateCheckout}>
             <h3 className='text-lg mb-4'>Contact Details</h3>
             <div className='mb-4'>
                 <label className='block text-gray-700'>Email</label>
@@ -76,6 +90,7 @@ const Checkout = () => {
     </div>
 
   </div>
+
   <div className='mb-4'>
     <label className='block text-gray-700'>
         Address
@@ -85,6 +100,75 @@ const Checkout = () => {
     className='w-full p-2 border rounded' required
     />
   </div>
+
+<div className='mb-4 grid grid-cols-2 gap-4'>
+<div>
+         <label className='block text-gray-700'>
+            City
+         </label>
+         <input 
+         type="text"
+         value={shippingAddress.city}
+         onChange={(e) => setShippingAddress({...shippingAddress, city: e.target.value})}
+        className='w-full p-2 border rounded'
+           required />
+
+    </div>
+
+    <div>
+         <label className='block text-gray-700'>
+            Postal Code
+         </label>
+         <input 
+         type="text"
+         value={shippingAddress.postalCode}
+         onChange={(e) => setShippingAddress({...shippingAddress, postalCode: e.target.value})}
+        className='w-full p-2 border rounded'
+           required />
+
+    </div>
+</div>
+
+
+<div className='mb-4'>
+    <label className='block text-gray-700'>
+        Country
+    </label>
+    <input type="text" value={shippingAddress.country} 
+    onChange={(e) => setShippingAddress({...setShippingAddress, country: e.target.value})}
+    className='w-full p-2 border rounded' required
+    />
+  </div>
+
+  
+  <div className='mb-4'>
+    <label className='block text-gray-700'>
+        Phone
+    </label>
+    <input type="tel" value={shippingAddress.phone} 
+    onChange={(e) => setShippingAddress({...setShippingAddress, phone: e.target.value})}
+    className='w-full p-2 border rounded' required
+    />
+  </div>
+
+  <div className='mt-6'>
+ {!checkoutId ? (
+    <button type='submit' className='w-full bg-black text-white py-3 rounded'>
+        Continue to Payment
+    </button>
+ ) : (
+    <div>
+        <h3 className='text-lg mb-4'>Pay with Paypal</h3>
+        {/* paypal component */}
+        <PayPalButton 
+        amount={100}
+        onSuccess={handlePaymentSuccess}
+        onError={(err) => alert("Payment failed. Try again.")}
+        />
+    </div>
+ )}
+  </div>
+
  </form>
          
     </div>       
