5ee542af3d2cfa27d435c207ed2c46adeb2085f3
saketkakkar-101 <saketkakkar55555@gmail.com>
2025-03-09 20:14:47 +0530

    commit

--- frontend/src/components/Cart/Checkout.jsx
+++ frontend/src/components/Cart/Checkout.jsx
@@ -37,7 +37,7 @@ const Checkout = () => {
 
     const handleCreateCheckout = (e) => {
         e.preventDefault();
-        // setCheckoutId(1234)
+        setCheckoutId(1234)
     }
 
 const handlePaymentSuccess = (details) => {
@@ -171,7 +171,31 @@ const handlePaymentSuccess = (details) => {
 
  </form>
          
-    </div>       
+    </div>  
+    {/* Right section */} 
+    <div className='bg-gray-50 p-6 rounded-lg'>
+        <h3 className='text-lg mb-4'>Order Summary</h3>
+        <div className='border-t py-4 mb-4'>
+            {cart.products.map((product, index) => (
+                <div key ={index} className='flex items-start justify-between py-2 border-b'>
+                    <div className='flex items-start'>
+                     <img 
+                     src={product.image}
+                     alt={product.name}
+                     className='w-20 h-24 object-cover mr-4'
+                     />
+                     <div>
+                        <h3 className='text-md'>{product.name}</h3>
+                        <p className='text-gray-500'>Size: {product.size}</p>
+                        <p className='text-gray-500'>Color: {product.color}</p>
+                     </div>
+                     
+                    </div>
+                    <p className='text-xl'>${product.price?.toLocaleString()}</p>
+                </div>
+            ))}
+        </div>
+        </div>    
     </div>
   )
 }
--- frontend/src/components/Cart/PayPalButton.jsx
+++ frontend/src/components/Cart/PayPalButton.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
-const PayPalButton = () => {
+import {PayPalButtons , PayPalScriptProvider} from "@paypal/react-paypal-js"
+
+const PayPalButton = ({amount, onSuccess, onError}) => {
   return (
-    <div>PayPalButton</div>
+    
+<PayPalScriptProvider options={{"clientId": 
+"AT4AEAU2nLngW_TLWFgdmeJrMA_2Hj4_LCmIrZDJX6NZT3AKzxJHsmqXWJreSeVxaTxzMR07ImwiJK_Q"}}>
+
+<PayPalButtons style={{layout : "vertical"}}
+createOrder={(data, actions) =>{
+  return actions.order.create({
+    purchase_units: [{amount: {value: amount}}]
+  })
+}}
+onApprove={(data , actions) => {
+  return actions.order.capture().then(onSuccess)
+}}
+onError={onError} />
+
+</PayPalScriptProvider>
+
   )
 }
 
