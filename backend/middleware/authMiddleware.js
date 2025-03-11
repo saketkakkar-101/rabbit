8b22664af9320db44377ad5e8b71edd650e70ff2
saketkakkar-101 <saketkakkar55555@gmail.com>
2025-03-12 01:09:17 +0530

    gg

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -21,6 +21,14 @@ const protect = async (req, res, next) => {
     } else {
         res.status(401).json({message: "Not authorized, no token provided"});
     }
+};
+
+const admin = (req, res, next) => {
+    if(req.user && req.user.role === "admin"){
+        next();
+    } else{
+        res.status(403).json({message: "Not authorized as an admin"});
+    }
 }
 
-module.exports = {protect};
+module.exports = {protect, admin};
--- backend/routes/productRoutes.jsx
+++ backend/routes/productRoutes.jsx
@@ -1,10 +1,10 @@
 const express = require("express");
 const Product = require("../models/Product");
-const { protect } = require("../middleware/authMiddleware")
+const { protect, admin} = require("../middleware/authMiddleware")
 
 const router = express.Router();
 
-router.post("/", protect, async (req, res) =>{
+router.post("/", protect, admin, async (req, res) =>{
     try{
         const {
         name,
@@ -27,6 +27,69 @@ router.post("/", protect, async (req, res) =>{
         weight,
         sku,
 
-        } = req.body
-    } catch(error) {}
+        } = req.body;
+
+           const product = new Product({
+            name,
+            description,
+            price,
+            discountPrice,
+            countInStock,
+            category,
+            brand,
+            sizes,
+            colors,
+            collections,
+            material,
+            gender,
+            images,
+            isFeatured,
+            isPublished,
+            tags,
+            dimensions,
+            weight,
+            sku,
+            user: req.user._id,
+            });
+            const createdProduct = await product.save();
+            res.status(201).json(createdProduct);
+
+    } catch(error) {
+        console.error(error);
+        res.status(500).send("Server Error");
+        
+    }
 })
+
+router.put("/:id", protect, admin, async(req,res) => {
+    try {
+        const {
+            name,
+            description,
+            price,
+            discountPrice,
+            countInStock,
+            category,
+            brand,
+            sizes,
+            colors,
+            collections,
+            material,
+            gender,
+            images,
+            isFeatured,
+            isPublished,
+            tags,
+            dimensions,
+            weight,
+            sku,
+    
+            } = req.body;
+
+            
+    } catch (error) {
+        
+    }
+})
+
+module.exports = router;
