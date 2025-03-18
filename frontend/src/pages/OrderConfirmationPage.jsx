c22eb0303ed89f7c987fcf0fc98deb4bb24cfac7
saketkakkar-101 <saketkakkar55555@gmail.com>
2025-03-18 18:27:37 +0530

    commit

--- frontend/src/pages/OrderConfirmationPage.jsx
+++ frontend/src/pages/OrderConfirmationPage.jsx
@@ -1,34 +1,6 @@
 import React from 'react'
 
-const checkout = {
-    _id: "12323",
-    createdAt: new Date(),
-    checkoutItems: [
-        {
-            productId: "1",
-            name: "Jacket",
-            color: "black",
-            size: "M",
-            price: 150,
-            quantity: 1,
-            image: "https://picsum.photos/150?random=1"
-        },
-        {
-            productId: "2",
-            name: "T-shirt",
-            color: "black",
-            size: "M",
-            price: 120,
-            quantity: 2,
-            image: "https://picsum.photos/150?random=2"
-        },
-    ],
-    shippingAddress: {
-        address: "123 Fashion Street",
-        city: "New York",
-        country: "USA"
-    }
-}
+
 
 const OrderConfirmationPage = () => {
  
--- frontend/src/redux/slices/authSlice.js
+++ frontend/src/redux/slices/authSlice.js
@@ -97,7 +97,7 @@ const authSlice = createSlice({
         })
         .addCase(registerUser.fulfilled, (state, action)=>{
             state.loading = false;
-            state.error = action.payload;
+            state.user = action.payload;
         })
         .addCase(registerUser.rejected, (state, action)=>{
             state.loading = false;
